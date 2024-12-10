edfb5bb85f83a6fba7fe073c9ad4f6fbfe74d21f
giorgiadifranco <giorgiadf08@gmail.com>
2024-12-10 16:13:37 +0100

    fix route id

--- beckend-api-server/routes/films.js
+++ beckend-api-server/routes/films.js
@@ -28,18 +28,28 @@ router.get('/:id', (req, res) =>{
     const id = req.params.id
     const sql = `SELECT * FROM movies WHERE id = ?`;
 
-    connection.query(sql, [id], (req, results) =>{
-        if (err) { 
-            return res.status(500).json({err: err})
-        }
-            console.log(results);
-
-            res.json({
-            movies: `returning the book with an id of ${id}`
-            
-    });
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ err: err })
+        if (results.length == 0) return res.status(404).json({ err: 'movie not found' })
+    
+        //console.log(results);
+    
+        // get all reviews associated to the book
+        connection.query(sql, [id], (err, results) => {
+          if (err) return res.status(500).json({ err: err })
+    
+          //console.log('reviews', reviewsResults);
+          const movie = {
+            ...results[0],
+           
+          }
+    
+          res.json(movie)
+    
+        })
+    
+      })
 });
 
-})
 
 module.exports = router;
