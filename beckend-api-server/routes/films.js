fe423d81689c55e13cae4c9806382e11d87b376a
giorgiadifranco <giorgiadf08@gmail.com>
2024-12-10 16:27:01 +0100

    add controllers

--- beckend-api-server/routes/films.js
+++ beckend-api-server/routes/films.js
@@ -1,55 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const connection = require ('../database/connection')
+
+const MovieControllers = require('../controllers/MovieControllers')
 
 
 //rotta per tutti i libri
 
-router.get('/', (req, res)=>{
+router.get('/', MovieControllers.index)
 
-    const sql = `SELECT * FROM movies`;
-
-    connection.query(sql, (err, results)=>{
-
-        console.log(results);
-        
-
-         res.json({
-            movies: results,
-            count: results.length
-         })
-    })
-
-   
-})
-
-router.get('/:id', (req, res) =>{
-    
-    const id = req.params.id
-    const sql = `SELECT * FROM movies WHERE id = ?`;
-
-    connection.query(sql, [id], (err, results) => {
-        if (err) return res.status(500).json({ err: err })
-        if (results.length == 0) return res.status(404).json({ err: 'movie not found' })
-    
-        //console.log(results);
-    
-        // get all reviews associated to the book
-        connection.query(sql, [id], (err, results) => {
-          if (err) return res.status(500).json({ err: err })
-    
-          //console.log('reviews', reviewsResults);
-          const movie = {
-            ...results[0],
-           
-          }
-    
-          res.json(movie)
-    
-        })
-    
-      })
-});
+router.get('/:id', MovieControllers.show);
 
 
 module.exports = router;
