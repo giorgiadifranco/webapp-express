f598d0acf79f3aeda766d52b6aebd43262a0a08e
giorgiadifranco <giorgiadf08@gmail.com>
2024-12-10 15:27:21 +0100

    fix films

--- beckend-api-server/routes/films.js
+++ beckend-api-server/routes/films.js
@@ -7,7 +7,7 @@ const connection = require ('../database/connection')
 
 router.get('/', (req, res)=>{
 
-    const sql = `SELECT * FROM films`;
+    const sql = `SELECT * FROM movies`;
 
     connection.query(sql, (err, results)=>{
 
