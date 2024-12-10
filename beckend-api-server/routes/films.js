5980fbb89934c7825aee34e70cab53d2a3db96be
giorgiadifranco <giorgiadf08@gmail.com>
2024-12-10 15:24:40 +0100

    add connection

--- beckend-api-server/routes/films.js
+++ beckend-api-server/routes/films.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const connection = require ('../database/connection')
 
 
 //rotta pèer tutti i libri
 
 router.get('/', (req, res)=>{
 
-    res.json({
+    const sql = `SELECT * FROM films`;
+
+    connection.query(sql, (err, results)=>{
+
+        console.log(results);
+        
+
+         res.json({
         films: 'all films'
     })
+
+    })
+
+   
 })
 
 module.exports = router;
